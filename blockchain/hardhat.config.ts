8609a242ef7a543bef36d6b4e4c9cd9d5e73877d
Subrat Samal <samalsubrat919@gmail.com>
2026-03-14 21:00:25 +0530

    sepolia // metamask // alchemy /// all are included

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "hardhat/config";
 import hardhatEthers from "@nomicfoundation/hardhat-ethers";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [hardhatEthers],
-  solidity: {
-    version: "0.8.28",
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      type: "http",
+      url: process.env.SEPOLIA_RPC_URL || "", // From Alchemy
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [], // From MetaMask
+    },
   },
 });
